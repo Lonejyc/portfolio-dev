f2f02fc33d8cf0f61508d23dccc5e45dfa702d17
Lonejyc <jocelyn.marcilloux@gmail.com>
2025-06-04 17:15:15 +0200

    fix: projects structure

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,41 +1,43 @@
 import { defineCollection, z } from 'astro:content';
 
-export const mmiCollections = {
-	workMmi: defineCollection({
-		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			img: z.string(),
-			img_alt: z.string().optional(),
-			support: z.array(z.string()),
-			git: z.string().optional(),
-		}),
-	}),
-};
-export const myroleCollections = {
-	workMyrole: defineCollection({
-		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			img: z.string(),
-			img_alt: z.string().optional(),
-			support: z.array(z.string()),
-			git: z.string().optional(),
-		}),
-	}),
-};
-export const persoCollections = {
-	workPerso: defineCollection({
-		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			img: z.string(),
-			img_alt: z.string().optional(),
-			support: z.array(z.string()),
-			git: z.string().optional(),
-		}),
-	}),
+const mmi = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        img: z.string(),
+        img_alt: z.string().optional(),
+        support: z.array(z.string()),
+        git: z.string().optional(),
+    }),
+});
+
+const myrole = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        img: z.string(),
+        img_alt: z.string().optional(),
+        support: z.array(z.string()),
+        git: z.string().optional(),
+    }),
+});
+
+const perso = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        img: z.string(),
+        img_alt: z.string().optional(),
+        support: z.array(z.string()),
+        git: z.string().optional(),
+    }),
+});
+
+export default {
+    mmi,
+    myrole,
+    perso,
 };
